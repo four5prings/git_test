86a064566a21aa34dd2148f41565e7732cd10711
four5prings <four5prings@163.com>
2022-06-14 20:23:24 +0800

    second commit

--- src/main/java/test.java
+++ src/main/java/test.java
@@ -2,5 +2,7 @@
 public class test {
     public static void main(String[] args) {
         System.out.println("helloworld");
+        System.out.println("helloworld1");
+        System.out.println("helloworld2");
     }
 }
